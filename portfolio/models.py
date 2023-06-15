a21b9c1bf5f0e90d0bc541872cd23496d0c51e3d
Luiza Vidal <lcopolillo@gmail.com>
2023-06-15 02:56:20 +0100

    inicia blog + post (django funciona)

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    autor = models.CharField(max_length=30)
+    data = models.DateField(auto_now_add=True)
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=5000)
+    link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo[: 20]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from .models import Post
 
 def home_page_view(request):
     return render(request, 'portfolio/index.html')
 
 def blog_page_view(request):
     return render(request, 'portfolio/blog.html')
+
+def post_page_view(request):
+    context = {'posts': Post.objects.all()}
+    return render(request, 'posts/posts.html', context)
